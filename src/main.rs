bd2d76fcefdf387f227c97b05f6679487df6f2a5
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2020-03-07 20:21:38 +0900

    Implement basic api

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-pub fn main() {
-    println!("Hello, world!");
-}
+extern crate reqwest;
 
+use scraper::{Html, Selector};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://trends24.in/japan/")
+        .await?
+        .text_with_charset("utf-8")
+        .await?;
+    //println!("{:#?}", resp);
+
+    let fragment = Html::parse_fragment(&resp);
+    let selector = Selector::parse("a").unwrap();
+    for element in fragment.select(&selector) {
+        println!("{:#?}", element.inner_html());
+    }
+
+    Ok(())
+}
 
 #[test]
 fn test_yes() {
     assert_eq!(true, true);
 }
-
